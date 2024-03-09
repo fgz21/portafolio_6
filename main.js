c5941da272bfb1a6e81929048b6c8771445688a8
fgz21 <flowhister30@gmail.com>
2024-03-09 00:16:41 -0400

    new update 09/03/2024

--- main.js
+++ main.js
@@ -22,3 +22,25 @@
   })
 
 
+
+
+// Seleccionar todos los elementos con el atributo 'title'
+const tooltips = document.querySelectorAll('[title]');
+
+// Agregar evento al hacer clic para copiar el correo electrónico
+tooltips.forEach((tooltip) => {
+  tooltip.addEventListener('click', () => {
+    const email = tooltip.querySelector('span').innerText.split(':')[1].trim();
+    
+    // Crear un elemento de texto temporal para copiar el correo electrónico
+    const tempInput = document.createElement('input');
+    tempInput.value = email;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    
+    // Mostrar mensaje de éxito
+    alert(`Correo electrónico copiado: ${email}`);
+  });
+});
